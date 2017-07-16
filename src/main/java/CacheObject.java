237e4190027f364dd1a62e3fe84800952315858d
alaincruzcasanova <alain.cruzc@gmail.com>
2017-07-15 19:02:49 -0600

    Agregamos las clases de Controller y Service

--- src/main/java/CacheObject.java
+++ src/main/java/CacheObject.java
@@ -1,24 +1,35 @@
 import java.util.*;
 
-/**
- * Created by alaincruzcasanova on 7/15/17.
- */
+// Abstract implementation of the cache
 public abstract class CacheObject<K,V> implements Cache<K,V> {
 
+    // Variables that will be needed for our cache settings.
+
+    // Max number of entries that the cache can hold
     private int maxEntries = 10;
+    // Max time that a entry can stay in the cache without being read
+    // Important, set to 0 to make it infinite
     private long entryMaxTime = 3600;
-    private long cacheMaxTime = 0;
+    // Given time for our cache to free all it's spaces
+    // Important, set to 0 to make it infinite
+    private long cacheMaxTime = 1000;
+    // Name of our cache
     private String cacheName = "";
-
+    // Priority queue that will measure when an entry's life has surpassed our max time
     private PriorityQueue<Entry> priorityQE;
-
+    // Key value map with all our entries
     private Map<K,Entry<K,V>> entryMap;
-
+    // Threads that will run the process of checking entries' and cache's life time
     private Thread entryThread;
     private Thread lifeThread;
 
+    /*
+    * Constructor #1
+    * Receive the name of our cache as a parameter
+    * */
     public CacheObject(String cacheName){
         this.cacheName = cacheName;
+        // For our priority queue we need to compare thier time in order to sort them
         Comparator<Entry> comparator = new EntryComparator();
         priorityQE = new PriorityQueue<Entry>(10, (Comparator<? super Entry>) comparator);
         entryMap = new HashMap<K, Entry<K, V>>();
@@ -26,14 +37,18 @@ public abstract class CacheObject<K,V> implements Cache<K,V> {
         initLifeTime(this);
     }
 
+    /*
+    * Constructor #1
+    * Receive the name of our cache as a parameter
+    * */
     public CacheObject(String cacheName, int maxEntries){
         this.cacheName = cacheName;
         this.maxEntries = maxEntries;
         Comparator<Entry> comparator = new EntryComparator();
         priorityQE = new PriorityQueue<Entry>(10, (Comparator<? super Entry>) comparator);
         entryMap = new HashMap<K, Entry<K, V>>();
-        initEntryLife(this);
-        initLifeTime(this);
+//        initEntryLife(this);
+//        initLifeTime(this);
     }
 
     public CacheObject(String cacheName, int maxEntries, long entryMaxTime, long cacheMaxTime){
@@ -44,8 +59,8 @@ public abstract class CacheObject<K,V> implements Cache<K,V> {
         Comparator<Entry> comparator = new EntryComparator();
         priorityQE = new PriorityQueue<Entry>(10, (Comparator<? super Entry>) comparator);
         entryMap = new HashMap<K, Entry<K, V>>();
-        initEntryLife(this);
-        initLifeTime(this);
+//        initEntryLife(this);
+//        initLifeTime(this);
     }
 
     public String getName() {
@@ -54,7 +69,8 @@ public abstract class CacheObject<K,V> implements Cache<K,V> {
 
     public synchronized V get(K key){
         if (entryMap.containsKey(key)) {
-            priorityQE.remove(key);
+            Entry oldEntry = entryMap.get(key);
+            priorityQE.remove(oldEntry);
             Entry entry = new Entry(key, getTime(), entryMap.get(key).getData());
             priorityQE.add(entry);
             return entryMap.get(key).getData();
@@ -91,14 +107,14 @@ public abstract class CacheObject<K,V> implements Cache<K,V> {
 
     public void initLifeTime(CacheObject cache) {
         if (cacheMaxTime>0) {
-            lifeThread = new Thread(new ThreadEntryLife(this));
+            lifeThread = new Thread(new ThreadLifeTime(this));
             lifeThread.start();
         }
     }
 
     public void initEntryLife(CacheObject cache){
         if (entryMaxTime>0) {
-            entryThread = new Thread(new ThreadLifeTime(this));
+            entryThread = new Thread(new ThreadEntryLife(this));
             entryThread.start();
         }
     };
